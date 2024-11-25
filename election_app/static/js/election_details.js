a254eca7503605acd308a4f625e8c8920485585b
Piz20 <eminiantpisani@gmail.com>
2024-11-25 16:38:28 +0100

    doing vote feature

--- election_app/static/js/election_details.js
+++ election_app/static/js/election_details.js
@@ -1,3 +1,61 @@
+// election_details.js
+
+
+
+const countdownTitle = document.getElementById('countdown-title');
+const countdownTimer = document.getElementById('countdown-timer');
+
+// Fonction pour formater le temps (jours, heures, minutes, secondes) - pas de décimales pour les secondes
+function formatTime(timeInSeconds) {
+    const days = Math.floor(timeInSeconds / (3600 * 24));
+    timeInSeconds -= days * 3600 * 24;
+    const hours = Math.floor(timeInSeconds / 3600);
+    timeInSeconds -= hours * 3600;
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = Math.floor(timeInSeconds % 60); // Utilisez Math.floor pour éliminer les décimales
+
+    return {
+        days: days,
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds
+    };
+}
+
+// Fonction pour mettre à jour le compte à rebours
+function updateCountdown() {
+    const now = new Date();
+    let timeRemaining;
+
+    if (now < electionStartDate) {
+        // L'élection n'a pas encore commencé
+        timeRemaining = (electionStartDate - now) / 1000; // en secondes
+        countdownTitle.innerText = "Time before election begins";
+        countdownTimer.style.color = "green"; // Définir la couleur du compte à rebours sur vert
+    } else if (now < electionEndDate) {
+        // L'élection est en cours
+        timeRemaining = (electionEndDate - now) / 1000; // en secondes
+        countdownTitle.innerText = "Time before election ends";
+        countdownTimer.style.color = "red"; // Définir la couleur du compte à rebours sur rouge
+    } else {
+        // L'élection est terminée
+        countdownTitle.innerText = "Election has ended";
+        countdownTimer.innerText = "";
+        return;
+    }
+
+    const time = formatTime(timeRemaining);
+
+    // Afficher le compte à rebours sans décimales pour les secondes
+    countdownTimer.innerText = `${time.days}d ${time.hours}h ${time.minutes}m ${time.seconds}s`;
+}
+
+// Mettre à jour le compte à rebours toutes les secondes
+setInterval(updateCountdown, 1000);
+updateCountdown(); // Initialiser immédiatement le compte à rebours
+
+
+
 // Function to open the modal for candidate creation
 function openModalCandidate() {
     // Get the modal element
